91cdb372b7a76b018fa8ea5a5e0f4c63072b0c55
PellucidMusic <99565991+PellucidMusic@users.noreply.github.com>
2022-02-19 18:01:08 +0000

    Update 2

--- Serial_Package.py
+++ Serial_Package.py
@@ -1,17 +1,25 @@
 import serial
 import time
-import struct
+
 
 port = "/dev/ttyACM0"
 
-array_a = [3,2,8,21,8,25,33,77,21,37,3,2,218,4,8,155,33,77,24,37,32,2,20,4,25,155,33,77,20,37,3,2,28,4,8,155,33,77,25,37,3,2,8,4,8,155,33,77,24,37,3,2,8,4,8,155,33,77,24]
-
+array_a = [21,21,21]
 
 SerialObj = serial.Serial(port) # COMxx   format on Windows
 SerialObj.baudrate = 115200  # set Baud rate to 9600
 SerialObj.bytesize = 8     # Number of data bits = 8
 SerialObj.parity   ='N'    # No parity
 SerialObj.stopbits = 1    # Number of Stop bits = 1
+
+
+def map_range(value, min_a, max_a , min_b = 0 , max_b = 255):    
+    span_a = max_a - min_a
+    span_b = max_b - min_b
+    valueScaled = min_b + ((float(value-min_a)/ float(span_a)) *span_b)
+    ret_value = int(valueScaled)
+    return(ret_value)
+
 
 
 
@@ -22,10 +30,11 @@ def send_serial(data_array):
     COBS_start = 255
     COBS_end = 255
     sum_check = (sum(data_array))%256
-    print(sum_check)
+    print(sum_check)  
+   
+    data_array, COBS_start = COBS(data_array, start_tag, start_tag, end_tag)
+    data_array, COBS_end = COBS(data_array, end_tag, start_tag, end_tag)  
     
-    data_array, COBS_start = COBS(data_array, start_tag)
-    data_array, COBS_end = COBS(data_array, end_tag)  
     print(COBS_start)
     print(COBS_end)
 
@@ -46,12 +55,13 @@ def send_serial(data_array):
     
 
 
-def COBS(in_array, search_t):
+def COBS(in_array, search_t,start_tag,end_tag):
     
     preceding_bytes = 3  # Number of bytes before the first payload
     COBS_byte = 255
     i = 0
     j = 0 
+    k = 0
     out_array = in_array
     COBS_pos = []
     
@@ -64,18 +74,25 @@ def COBS(in_array, search_t):
 
     if (len(COBS_pos) !=  0):
         COBS_byte = COBS_pos[0]+preceding_bytes
-    
+        if (COBS_byte == start_tag):
+            COBS_byte = 0 
+        if (COBS_byte == end_tag):
+            COBS_byte = 1 
         
-    while j < (len(COBS_pos)):         
-        
+    while j < (len(COBS_pos)):                
         if (j == ((len(COBS_pos))-1)):
             out_array[(COBS_pos[j])] = 255
             j+= 1
         else: 
             out_array[(COBS_pos[j])] = COBS_pos[j+1]+preceding_bytes 
+            if (out_array[(COBS_pos[j])] == start_tag):
+                out_array[(COBS_pos[j])] = 0 
+            if (out_array[(COBS_pos[j])] == end_tag):
+                out_array[(COBS_pos[j])] = 1 
+            
             j += 1
-        
 
+    
     return(out_array, COBS_byte)
 
 
